f116801943e16042ee3cfc95dddd29b2d5d790b2
Md.Ariful Islam Fahim <43854345+FahimDev@users.noreply.github.com>
2020-06-28 19:16:52 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# glitchPortal-MobileApplication-Flutter
+ 
---
+++ android/app/src/main/kotlin/com/example/glitchApp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.glitchApp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
