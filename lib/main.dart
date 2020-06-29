fee647e372c4d571c47d57656fcb7248683ac611
Md.Ariful Islam Fahim <43854345+FahimDev@users.noreply.github.com>
2020-06-29 13:34:43 +0600

    Login Button developped

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:flutter/services.dart'; //For preventing Rotation
 
 void main() {
   runApp(MyApp());
@@ -8,16 +9,20 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: loginPage());
+    return MaterialApp(debugShowCheckedModeBanner: false, home: LoginPage());
   }
 }
 
-class loginPage extends StatelessWidget {
+class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var screenHeight = MediaQuery.of(context).size.height;
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return Scaffold(
-      backgroundColor: Colors.blueGrey,
+      backgroundColor: Colors.transparent,
       resizeToAvoidBottomPadding: false,
       body: Container(
         decoration: BoxDecoration(
@@ -31,119 +36,136 @@ class loginPage extends StatelessWidget {
                 Color(0xFF398AE5),
               ]),
         ),
-        child: GestureDetector(
-          child: Hero(
-            tag: 'login',
-            child: Column(
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: screenHeight / 9,
+            ),
+            Stack(
+              alignment: Alignment.center,
               children: <Widget>[
                 Container(
-                  height: screenHeight / 9,
-                ),
-                Stack(
-                  alignment: Alignment.center,
-                  children: <Widget>[
-                    Container(
-                      child: CircleAvatar(
-                        radius: 50,
-                      ),
-                    ),
-                    Positioned(
-                      child: Container(
-                        child: IconButton(
-                          icon: FaIcon(
-                            FontAwesomeIcons.userAstronaut,
-                            color: Colors.white70,
-                          ),
-                          iconSize: 70,
-                          onPressed: null,
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-                Container(
-                  child: Text(
-                    "Login",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontFamily: 'Raleway',
-                      fontSize: 30.0,
-                      //fontWeight: FontWeight.bold,
-                    ),
+                  child: CircleAvatar(
+                    radius: 50,
                   ),
                 ),
-                Expanded(
+                Positioned(
                   child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.white10,
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(50),
-                        topRight: Radius.circular(50),
+                    child: IconButton(
+                      icon: FaIcon(
+                        FontAwesomeIcons.userAstronaut,
+                        color: Colors.white70,
                       ),
+                      iconSize: 70,
+                      onPressed: null,
                     ),
-                    child: Padding(
-                      padding: EdgeInsets.all(20),
-                      child: Column(
-                        children: <Widget>[
-                          Container(
-                            padding: EdgeInsets.all(20),
-                            decoration: BoxDecoration(
-                              color: Colors.white24,
-                              borderRadius: BorderRadius.only(
-                                topLeft: Radius.circular(30),
-                                topRight: Radius.circular(30),
-                              ),
-                              boxShadow: [
-                                BoxShadow(
-                                  color: Colors.black12,
-                                  blurRadius: 20,
-                                  offset: Offset(0, 10),
-                                ),
-                              ],
-                            ),
-                            child: Column(
-                              children: <Widget>[
-                                TextField(
-                                  decoration:
-                                      InputDecoration(hintText: "User Name"),
-                                )
-                              ],
-                            ),
+                  ),
+                )
+              ],
+            ),
+            Container(
+              child: Text(
+                "Login",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontFamily: 'Raleway',
+                  fontSize: 30.0,
+                  //fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white10,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(50),
+                    topRight: Radius.circular(50),
+                  ),
+                ),
+                child: Padding(
+                  padding: EdgeInsets.all(20),
+                  child: Column(
+                    children: <Widget>[
+                      Container(
+                        padding: EdgeInsets.all(20),
+                        decoration: BoxDecoration(
+                          color: Colors.white24,
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(30),
+                            topRight: Radius.circular(30),
                           ),
-                          Container(
-                            padding: EdgeInsets.all(20),
-                            decoration: BoxDecoration(
-                              color: Colors.white24,
-                              borderRadius: BorderRadius.only(
-                                bottomLeft: Radius.circular(20),
-                                bottomRight: Radius.circular(10),
-                              ),
-                              boxShadow: [
-                                BoxShadow(
-                                  color: Colors.black12,
-                                  blurRadius: 20,
-                                  offset: Offset(0, 10),
-                                ),
-                              ],
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black12,
+                              blurRadius: 20,
+                              offset: Offset(0, 10),
                             ),
-                            child: Column(
-                              children: <Widget>[
-                                TextField(
-                                  decoration:
-                                      InputDecoration(hintText: "Password"),
-                                )
-                              ],
+                          ],
+                        ),
+                        child: Column(
+                          children: <Widget>[
+                            TextField(
+                              decoration:
+                                  InputDecoration(hintText: "User Name"),
+                            )
+                          ],
+                        ),
+                      ),
+                      Container(
+                        padding: EdgeInsets.all(20),
+                        decoration: BoxDecoration(
+                          color: Colors.white24,
+                          borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(20),
+                            bottomRight: Radius.circular(10),
+                          ),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black12,
+                              blurRadius: 20,
+                              offset: Offset(0, 10),
                             ),
-                          ),
-                          SizedBox(
-                            height: 20,
-                          ),
-                          Container(
+                          ],
+                        ),
+                        child: Column(
+                          children: <Widget>[
+                            TextField(
+                              decoration: InputDecoration(hintText: "Password"),
+                            )
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Container(
+                        child: GestureDetector(
+                          onTap: () {
+                            BoxDecoration(color: Colors.black12);
+                          },
+                          child: Container(
                             height: 50,
-                            margin: EdgeInsets.symmetric(horizontal: 50),
+                            margin: EdgeInsets.symmetric(horizontal: 30),
                             decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(50),
-                                color: Colors.lightBlueAccent),
+                              borderRadius: BorderRadius.circular(20),
+                              gradient: LinearGradient(
+                                  begin: Alignment.topCenter,
+                                  end: Alignment.bottomCenter,
+                                  colors: [
+                                    Color(0xFF73AEF5),
+                                    Color(0xFF61A4F1),
+                                    Color(0xFF478DE0),
+                                    Color(0xFF398AE5),
+                                  ]),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.black38,
+                                  blurRadius: 20,
+                                  offset: Offset(0, 10),
+                                ),
+                              ],
+                            ),
                             child: Center(
                               child: Text(
                                 "Login",
@@ -153,14 +175,14 @@ class loginPage extends StatelessWidget {
                               ),
                             ),
                           ),
-                        ],
+                        ),
                       ),
-                    ),
+                    ],
                   ),
                 ),
-              ],
+              ),
             ),
-          ),
+          ],
         ),
       ),
     );
